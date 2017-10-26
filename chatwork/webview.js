5f36b3e396344c4f10a7ce02f1eed67d936fd7f4
koma <mkoma@K.local>
2017-10-26 19:04:27 +0900

    Fix the counting method

--- chatwork/webview.js
+++ chatwork/webview.js
@@ -1,14 +1,29 @@
 module.exports = (Franz, options) => {
   function getMessages() {
-    let count = 0;
+    let directCount = 0;
     let indirectCount = 0;
-    let badge = document.getElementById('_chatUnreadStatus');
+    let elements = document.getElementsByClassName('roomListBadges__unreadBadge _unreadBadge');
 
-    if (badge && badge.innerText) {
-        indirectCount = parseInt(badge.innerText);
-    }
+    Array.prototype.forEach.call(elements, function(item) {
+      let count = 0;
 
-    Franz.setBadge(count, indirectCount);
+      isGroup = item.parentNode.parentNode.parentNode.getElementsByClassName("avatarGroup").length
+      if (item.innerText) {
+        count = parseInt(item.innerText);
+      }
+
+      if (isGroup) {
+        // Count incoming group messages as indirectCount
+        indirectCount += count;
+      } else {
+        directCount += count;
+      }
+    });
+
+    // Current Franz 5 cannot show both of directCount and indirectCount on the badge.
+    // So, pass the summation of them to setBadge function.
+    // I'll change this code if Franz 5 have some options (e.g. switch for counting method) in the future.
+    Franz.setBadge(directCount + indirectCount);
   }
 
   Franz.loop(getMessages);
